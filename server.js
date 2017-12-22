9c5240509ead03901d4c122c389e189850b4223f
Lisa Ewart <ewartle@gmail.com>
2017-12-21 23:26:17 -0500

    fixes connection between front end and back end, but does not address clear table from backend or the issue of alerting when on the waitlist

--- server.js
+++ server.js
@@ -4,15 +4,24 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
 
-// Sets up the Express App
+// Sets up the Express App -- I ADDED BELOW TO MAKE SURE IT WILL WORK WITH HEROKU
 // =============================================================
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // Table reservations (DATA)
 // =============================================================
-var tables = [{
+
+var tables = [];
+var waitlist = [];
+
+
+//NOTE:  I added the "1" to tables and waitlist to work with empty arrays.  Remove the 1 if we want to work with full arrays.
+var tables1 = [{
         customerName: "Ahmed",
         customerEmail: "afhaque89@gmail.com",
         customerID: "afhaque89",
@@ -44,7 +53,7 @@ var tables = [{
     }
 ];
 
-var waitlist = [{
+var waitlist1 = [{
         customerName: "Ahmed",
         customerEmail: "afhaque89@gmail.com",
         customerID: "afhaque89",
@@ -54,19 +63,19 @@ var waitlist = [{
         customerName: "John Liscar",
         phoneNumber: "7065706063",
         customerEmail: "john.liscar@gmail.com",
-        customerID: ""
+        customerID: "32"
     },
     {
         customerName: "Austin Branham",
         phoneNumber: "7708837281",
         customerEmail: "austinhb1993@gmail.com",
-        customerID: ""
+        customerID: "43"
     },
     {
         customerName: "Rick James",
         phoneNumber: "555 555 5555",
         customerEmail: "rickjames@rickjames.com",
-        customerID: ""
+        customerID: "55"
     },
     {
         customerName: "Miki",
@@ -100,24 +109,33 @@ app.get("/api/waitlist", function(req, res) {
 
 // Create New Reservations - takes in JSON input
 app.post("/api/new", function(req, res) {
-    // req.body hosts is equal to the JSON post sent from the user
-    // This works because of our body-parser middleware
-    var newreservation = req.body
-    // Using a RegEx Pattern to remove spaces from newCharacter
-    if (tables.length < 2) {
-        tables.push(newreservation);
-        res.json(true);
-        console.log(true);
-    } else {
-        waitlist.push(newreservation);
-        res.json(false);
-        console.log(false);
-    }
 
-    console.log(waitlist);
-    console.log(tables);
+    var newReservation = req.body
+    
+    // Using a RegEx Pattern to remove spaces from newCharacter
+    newReservation.routeName = newReservation.customerName.replace(/\s+/g, "").toLowerCase();
+    console.log(newReservation);
+   
+
+    if (tables.length < 5) {
+         tables.push(newReservation);
+         console.log(true);
+         return res.json(true);
+         
+      } else {
+          waitlist.push(newReservation);
+          console.log(false);
+          return res.json(false);
+         
+       }
+    
 
 });
+
+
+
+
+
 
 // Starts the server to begin listening
 // =============================================================
